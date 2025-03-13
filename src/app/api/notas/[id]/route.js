9c8ca835837e1f0c196d44461a59d4d201a380f6
Ismael-Rvas <ismael.rivas.developer@gmail.com>
2025-03-13 18:45:16 +0100

    actudd

--- src/app/api/notas/[id]/route.js
+++ src/app/api/notas/[id]/route.js
@@ -1,44 +1,44 @@
 import { connectToDatabase } from "@/lib/mongodb";
-import { ObjectId } from "mongodb"; 
-
-export async function OPTIONS() {
-    return new Response(null, { status: 204 });
-}
-
-export async function GET(request, { params }) {
-    const { database } = await connectToDatabase();
-    const collection = database.collection(process.env.MONGODB_COLLECTION);
-
-    const { id } = await params
-    const results = await collection.find({ _id: new ObjectId(id) }).toArray();
-
-    return new Response(JSON.stringify(results[0]));
-}
-
-
-export async function PUT(request, { params }) {
-    const content = request.headers.get('content-type')
-
-    if (content != 'application/json')
-        return Response.json({ message: 'Debes proporcionar datos JSON' })
-
-    const { database } = await connectToDatabase();
-    const collection = database.collection(process.env.MONGODB_COLLECTION);
-
-    const { id } = await params
-    const {   title, descripcion, img, active } = await request.json() // Read body request
-    const results = await collection.updateOne({ _id: new ObjectId(id) }, { $set: {  title, descripcion, img, active } });
-
-    return Response.json(results);
-}
-
-
-export async function DELETE(request, { params }) {
-    const { database } = await connectToDatabase();
-    const collection = database.collection(process.env.MONGODB_COLLECTION);
-
-    const { id } = await params
-    const results = await collection.deleteOne({ _id: new ObjectId(id) })
-
-    return Response.json(results);
-}
+ import { ObjectId } from "mongodb"; 
+ 
+ export async function OPTIONS() {
+     return new Response(null, { status: 204 });
+ }
+ 
+ export async function GET(request, { params }) {
+     const { database } = await connectToDatabase();
+     const collection = database.collection(process.env.MONGODB_COLLECTION);
+ 
+     const { id } = await params;
+     const results = await collection.find({ _id: new ObjectId(id) }).toArray();
+ 
+     return new Response(JSON.stringify(results[0]));
+ }
+ 
+ 
+ export async function PUT(request, { params }) {
+     const content = request.headers.get('content-type')
+ 
+     if (content != 'application/json')
+         return Response.json({ message: 'Debes proporcionar datos JSON' })
+ 
+     const { database } = await connectToDatabase();
+     const collection = database.collection(process.env.MONGODB_COLLECTION);
+ 
+     const { id } = await params
+     const { title, descripcion, img, active } = await request.json() // Read body request
+     const results = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { title, descripcion, img, active } });
+ 
+     return Response.json(results);
+ }
+ 
+ 
+ export async function DELETE(request, { params }) {
+     const { database } = await connectToDatabase();
+     const collection = database.collection(process.env.MONGODB_COLLECTION);
+ 
+     const { id } = await params
+     const results = await collection.deleteOne({ _id: new ObjectId(id) })
+ 
+     return Response.json(results);
+ }
--- src/app/api/notas/route.js
+++ src/app/api/notas/route.js
@@ -1,38 +1,37 @@
 import { connectToDatabase } from "@/lib/mongodb";
-
-const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type"
-};
-
-export async function OPTIONS() {
-    return new Response(null, { status: 204, headers: corsHeaders });
-}
-
-
-export async function GET(request) {
-    const { database } = await connectToDatabase();
-    const collection = database.collection(process.env.MONGODB_COLLECTION);
-
-    const results = await collection.find({}).toArray();
-
-    return Response.json(results,{headers: corsHeaders});
-}
-
-export async function POST(request) {
-    const content = request.headers.get('content-type')
-
-    if (content != 'application/json')
-        return Response.json({ message: 'Debes proporcionar datos JSON' },
-    { headers: corsHeaders });
-
-    const { database } = await connectToDatabase();
-    const collection = database.collection(process.env.MONGODB_COLLECTION)
-
-    const {  title, descripcion, img, active } = await request.json() // Read body request
-    const results = await collection.insertOne({  title, descripcion, img, active });
-
-    return Response.json(results, { headers: corsHeaders });
-}
-
+ 
+ const corsHeaders = {
+     "Access-Control-Allow-Origin": "*",
+     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+     "Access-Control-Allow-Headers": "Content-Type"
+ };
+ 
+ export async function OPTIONS() {
+     return new Response(null, { status: 204, headers: corsHeaders });
+ }
+ 
+ export async function GET(request) {
+     const { database } = await connectToDatabase();
+     const collection = database.collection("notas");
+ 
+     const results = await collection.find({}).toArray();
+ 
+     return Response.json(results,{headers: corsHeaders});
+ }
+ 
+ export async function POST(request) {
+     const content = request.headers.get('content-type')
+ 
+     if (content !== 'application/json')
+         return Response.json({ message: 'Debes proporcionar datos JSON' },
+     { headers: corsHeaders });
+ 
+     const { database } = await connectToDatabase();
+     const collection = database.collection('notas');
+ 
+     const { title, descripcion, img, active } = await request.json() 
+     const results = await collection.insertOne({ title, descripcion, img, active});
+ 
+     return Response.json(results, { headers: corsHeaders });
+     
+ }
